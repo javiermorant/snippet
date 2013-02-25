90d85f53436eec6393bbb757985988365207055e
javier morant <javiermorant@gmail.com>
2013-02-25 16:13:07 +0100

    Singleton pattern

---
+++ Patterns/src/test/snippet/patterns/creational/singleton/Singleton.java
@@ -0,0 +1,12 @@
+package test.snippet.patterns.creational.singleton;
+
+public class Singleton {
+	private static Singleton instance = new Singleton();
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		return instance;
+	}
+}
